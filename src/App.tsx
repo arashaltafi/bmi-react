6e30a6866d454a4c13635f177b9ce17128b9f3ad
آرش الطافي <0021328374@nahad.ir>
2023-12-31 12:40:02 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,18 @@ const App = () => {
   }, [])
 
   return (
-    <Routes>
-      <Route element={<ScrollToTop />}>
-        <Route element={<Hoc />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/result" element={<Result />} />
-          <Route path="*" element={<NotFound />} />
+    <div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-screen mx-auto">
+      <Routes>
+        <Route element={<ScrollToTop />}>
+          <Route element={<Hoc />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/info" element={<Info />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </div>
   )
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router";
 import locationSlice from "../redux/locationSlice";
 import { Link } from "react-router-dom";
+import { IoIosFemale } from "react-icons/io";
+import { IoIosMale } from "react-icons/io";
 
 const Home = () => {
   const location = useLocation();
@@ -20,11 +22,36 @@ const Home = () => {
     navigate('/info')
   }
 
+  const changeTheme = () => {
+    if (localStorage.getItem('theme') === 'light') {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark')
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  }
+
   return (
-    <div className="div-col">
-      <h1 className="h1">Home</h1>
-      <button onClick={handleToNavigate}>Navigate To Info</button>
-      <Link to='/result'>Link To Result</Link>
+    <div className="bg-slate-100 dark:bg-slate-950 py-4 px-4">
+      <h1 className="h2">BMI Calculator</h1>
+      <div className="w-full h-[calc(100vh-6rem)] grid grid-cols-2 grid-rows-5 gap-x-4 gap-y-4">
+        <div className="bg-item flex items-center justify-center gap-x-4">
+          <IoIosMale />
+          <p className="h2">Male</p>
+        </div>
+        <div className="bg-item flex items-center justify-center gap-x-4">
+          <IoIosFemale />
+          <p className="h2">Female</p>
+        </div>
+        <div className="bg-item row-span-4">Weight</div>
+        <div className="bg-item row-span-2">Height</div>
+        <div className="bg-item row-span-2">Age</div>
+        <button onClick={changeTheme} className="btn-primary col-span-2">Let's Begin</button>
+      </div>
     </div>
   )
 }
