dcbee40088f00e093052819163297ecca3820635
Arash Altafi <arashaltafi1377@gmail.com>
2024-01-01 01:02:55 +0330

    a

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -4,7 +4,9 @@ import { useLocation, useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import bmiSlice from "../redux/BmiSlice";
-import { IoIosArrowBack } from "react-icons/io";
+import { FaAngleRight } from "react-icons/fa6";
+import { MdOutlineNightlight } from "react-icons/md";
+import { WiDaySunny } from "react-icons/wi";
 
 const Result = () => {
   const location = useLocation();
@@ -20,6 +22,19 @@ const Result = () => {
     }]));
   }, [])
 
+  const changeTheme = () => {
+    if (localStorage.getItem('theme') === 'light') {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark')
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  }
+
   const handleToNavigate = () => {
     dispatch(bmiSlice.actions.setResult(''));
     navigate('/info')
@@ -32,16 +47,24 @@ const Result = () => {
 
   return (
     <div className="w-full h-screen flex flex-col gap-4 bg-slate-100 dark:bg-slate-900 py-4 px-4">
-      <div className="flex flex-row items-center justify-between mx-4">
-        <span></span>
-        <h1 className="h2">{t('bmi_result')}</h1>
-        <IoIosArrowBack onClick={handleNavigateBack} className='bg-action w-12 h-12' />
+      <div className="div-row mx-4">
+        <FaAngleRight onClick={handleNavigateBack} className='bg-action' />
+        <h1 className="h2 flex-1">{t('bmi_result')}</h1>
+        <div className="flex flex-row">
+          <MdOutlineNightlight onClick={changeTheme} className='bg-action dark:hidden' />
+          <WiDaySunny onClick={changeTheme} className='bg-action hidden dark:block' />
+        </div>
       </div>
 
-      <div className="flex flex-col flex-1">
-        <h3 className="h3">{t('bmi_result')}</h3>
-        <p className="p">{bmiSelector.result}</p>
-      </div>
+      <section className="flex flex-col flex-1 gap-12 items-center justify-center">
+        <div className="flex items-center justify-center w-72 h-72 rounded-full shadow-2xl shadow-slate-300 dark:shadow-slate-950">
+          <div className="flex items-center justify-center w-64 h-64 border-[16px] border-solid border-amber-400 rounded-full shadow-lg shadow-amber-500">
+            <p className="h1">26.2</p>
+          </div>
+        </div>
+
+        <p className="h5">{t('bmi_result_text')}</p>
+      </section>
 
       <button onClick={handleToNavigate} className="btn-primary col-span-2 text-xl">
         {t('show_info')}
