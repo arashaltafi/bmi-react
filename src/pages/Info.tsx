81a2762daa9c5fa2d99417335f1fa3b657100506
Arash Altafi <arashaltafi1377@gmail.com>
2024-01-01 01:07:02 +0330

    a

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import locationSlice from "../redux/locationSlice";
+import { useTranslation } from "react-i18next";
+import { FaAngleRight } from "react-icons/fa6";
+import { MdOutlineNightlight } from "react-icons/md";
+import { WiDaySunny } from "react-icons/wi";
+
 
 const Info = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
     dispatch(locationSlice.actions.addLocation([{
@@ -14,8 +21,35 @@ const Info = () => {
     }]));
   }, [])
 
+  const changeTheme = () => {
+    if (localStorage.getItem('theme') === 'light') {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark')
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  const handleNavigateBack = () => {
+    navigate('/result')
+  }
+
   return (
-    <h1 className="h1">Info</h1>
+    <div className="w-full h-screen flex flex-col gap-4 bg-slate-100 dark:bg-slate-900 py-4 px-4">
+      <div className="div-row mx-4">
+        <FaAngleRight onClick={handleNavigateBack} className='bg-action' />
+        <h1 className="h2 flex-1">{t('bmi_info')}</h1>
+        <div className="flex flex-row">
+          <MdOutlineNightlight onClick={changeTheme} className='bg-action dark:hidden' />
+          <WiDaySunny onClick={changeTheme} className='bg-action hidden dark:block' />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
