2d31734aac67dc4c1aeeba295c8fe100295c6828
Arash Altafi <arashaltafi1377@gmail.com>
2024-01-01 00:34:18 +0330

    a

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import locationSlice from "../redux/locationSlice";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import bmiSlice from "../redux/BmiSlice";
+import { IoIosArrowBack } from "react-icons/io";
 
 const Result = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const bmiSelector = useSelector((state: any) => state.bmi);
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
     dispatch(locationSlice.actions.addLocation([{
@@ -15,9 +20,31 @@ const Result = () => {
     }]));
   }, [])
 
-  return (
-    <div className="div-col">
+  const handleToNavigate = () => {
+    dispatch(bmiSlice.actions.setResult(''));
+    navigate('/info')
+  }
 
+  const handleNavigateBack = () => {
+    dispatch(bmiSlice.actions.setResult(''));
+    navigate('/home')
+  }
+
+  return (
+    <div className="flex flex-col gap-4 bg-slate-100 dark:bg-slate-900 py-4 px-4">
+      <div className="flex flex-row items-center justify-between mx-4">
+        <span></span>
+        <h1 className="h2">{t('bmi_result')}</h1>
+        <IoIosArrowBack onClick={handleNavigateBack} className='bg-action w-12 h-12' />
+      </div>
+
+      <div className="flex flex-col">
+
+      </div>
+
+      <button onClick={handleToNavigate} className="btn-primary col-span-2 text-xl">
+        {t('show_info')}
+      </button>
     </div>
   )
 }
