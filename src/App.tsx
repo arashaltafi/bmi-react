473d9413e358c5fcde836ed2c4638e59ac6d6a71
آرش الطافي <0021328374@nahad.ir>
2023-12-31 10:00:40 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-function App() {
+import { useEffect } from "react";
+import { Provider } from "react-redux"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { store } from "./redux/Store";
+import ScrollToTop from "./Hoc/ScrollToTop";
+import Hoc from "./Hoc/Hoc";
+import Home from "./components/Home";
+
+const App = () => {
+
+  useEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else if (localStorage.getItem('theme') === 'light') {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Router basename='/'>
+      <Provider store={store}>
+        <Routes>
+          <Route element={<ScrollToTop />}>
+            <Route element={<Hoc />}>
+              <Route path='/' element={<Home />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Provider>
+    </Router>
   )
 }
 
---
+++ src/redux/numberSlice.ts
@@ -0,0 +1,47 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+interface NumberState {
+    number: string,
+    result: string,
+}
+
+const initialState: NumberState = {
+    number: '',
+    result: ''
+}
+
+const numberSlice = createSlice({
+    name: 'numbers',
+    initialState,
+    reducers: {
+        setNumber: (state, action: PayloadAction<string>) => {
+            if (state.number.length < 15) {
+                state.number += action.payload;
+            }
+        },
+        setSymbol: (state, action: PayloadAction<string>) => {
+            const symbols = ['+', '-', '*', '/'];
+            if (!symbols.includes(state.number.slice(-1)) && state.number.slice(-1) !== "" && state.number.slice(-1) !== ".") {
+                state.number = state.number + action.payload;
+            }
+        },
+        calculate: (state) => {
+            state.result = eval(state.number).toString();
+        },
+        deleteNumber: (state) => {
+            if (state.number.slice(-1) === " ") {
+                state.number = state.number.substring(0, state.number.length - 3);
+            } else if (state.number.slice(-2) === "0.") {
+                state.number = state.number.substring(0, state.number.length - 2);
+            } else {
+                state.number = state.number.substring(0, state.number.length - 1);
+            }
+        },
+        clearNumber: (state) => {
+            state.number = '';
+            state.result = '';
+        }
+    },
+})
+
+export default numberSlice
