13454c17d739c6d4ccaea8b2514d56cf380f86dc
Arash Altafi <arashaltafi1377@gmail.com>
2024-01-02 20:45:33 +0330

    a

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 const NotFound = () => {
+  const { t } = useTranslation();
+
   return (
-    <div>NotFound</div>
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="h1">{t('not_found')}</h1>
+    </div>
   )
 }
 
