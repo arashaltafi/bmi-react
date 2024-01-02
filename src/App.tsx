f9bab0e82a073754799022c0e6a6e3fc711270e3
آرش الطافي <0021328374@nahad.ir>
2024-01-02 12:02:37 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from 'react-router-dom';
 import ScrollToTop from "./Hoc/ScrollToTop";
 import Hoc from "./Hoc/Hoc";
@@ -11,6 +11,8 @@ import { useTranslation } from "react-i18next";
 
 const App = () => {
   const { i18n } = useTranslation();
+  const [orientation, setOrientation] = useState<number | null>(null);
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (localStorage.getItem('theme') === 'dark') {
@@ -27,20 +29,29 @@ const App = () => {
       document.body.classList.remove('font-serif')
       document.body.classList.add('font-vazir')
     }
+
+    setOrientation(window.orientation);
+    window.addEventListener('orientationchange', () => {
+      setOrientation(window.orientation);
+    });
   }, [])
 
   return (
     <div className="w-full md:w-[70%] lg:w-1/2 xl:w-[50%] 2xl:w-[40%] h-screen mx-auto overflow-hidden ios-padding">
-      <Routes>
-        <Route element={<ScrollToTop />}>
-          <Route element={<Hoc />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/info" element={<Info />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="*" element={<NotFound />} />
+      {orientation ? (
+        <p className="h1 w-full h-full flex items-center justify-center">{t('rotate')}</p>
+      ) : (
+        <Routes>
+          <Route element={<ScrollToTop />}>
+            <Route element={<Hoc />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/info" element={<Info />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      )}
     </div>
   )
 }
